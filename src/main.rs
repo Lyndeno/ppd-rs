691abb04f5476d26c1d746f94dbf662fe98cebae
Lyndon Sanche <lsanche@lyndeno.ca>
2025-04-17 10:47:14 -0600

    Add set method

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use zbus::{Connection, Result, proxy};
 fn main() {
     let result = futures::executor::block_on(get_profile()).unwrap();
     println!("Current profile is {result}");
+    futures::executor::block_on(set_profile("performance".to_string())).unwrap();
+    let result = futures::executor::block_on(get_profile()).unwrap();
+    println!("Current profile is {result}");
 }
 
 #[proxy(
@@ -12,6 +15,9 @@ fn main() {
 trait Ppd {
     #[zbus(property)]
     fn active_profile(&self) -> Result<String>;
+
+    #[zbus(property)]
+    fn set_active_profile(&self, string: String) -> Result<()>;
 }
 
 async fn get_profile() -> Result<String> {
@@ -20,4 +26,12 @@ async fn get_profile() -> Result<String> {
     let proxy = PpdProxy::new(&connection).await?;
     let reply = proxy.active_profile().await?;
     Ok(reply)
+}
+
+async fn set_profile(string: String) -> Result<()> {
+    let connection = Connection::system().await?;
+
+    let proxy = PpdProxy::new(&connection).await?;
+    proxy.set_active_profile(string).await?;
+    Ok(())
 }
